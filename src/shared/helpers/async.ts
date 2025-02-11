38e724ea64a6ae8914f29d3127245d9c728d1c2b
Fernando Couto <fernando.avcouto@gmail.com>
2025-02-11 18:58:41 -0300

    add cache

--- src/shared/helpers/async.ts
+++ src/shared/helpers/async.ts
@@ -1,8 +1,5 @@
-const min = 1000;
-const max = 4000;
-
 export function fakeRandomDelay() {
   return new Promise((resolve) => {
-    setTimeout(resolve, Math.floor(Math.random() * (max - min + 1)) + min);
+    setTimeout(resolve, 2000);
   });
 }
